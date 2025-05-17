5e102e1f7dbce8f2b52424f7eabf58ac10d381cc
Sattyik Kundu <sattyikkundu@gmail.com>
2025-05-16 22:13:32 -0400

    Added express.static() to enable access to server/public/images to use product images in front-end product cards.

--- server/server.js
+++ server/server.js
@@ -15,6 +15,9 @@ app.use(express.json());     // tell express to parse ANY JSON data in the body
                              // of incoming requests and conver that json data into 
                              // javascript object(s) the can be used via route handlers.
 
+// Serves static images in 'public/images/' folder (MUST USE to access products' images!!!)
+app.use('/images', express.static('public/images'));
+
 //Mount product routes (for handling product-related requests)
 app.use('/',productRoutes); // all routes from productRoutes are prefixed with '/' (e.g. '/products' , '/products/fishes')
 
