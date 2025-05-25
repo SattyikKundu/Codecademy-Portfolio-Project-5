ffee7e05fbebc4ecce727cc6db800b50aaa3e4d7
Sattyik Kundu <sattyikkundu@gmail.com>
2025-05-24 22:06:41 -0400

    Updated icon for eventual user profile/login page in header menu

--- client/src/PageComponents/menuUserProfile/menuUserProfile.jsx
+++ client/src/PageComponents/menuUserProfile/menuUserProfile.jsx
@@ -1,5 +1,9 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // used to import FontAwesomeIcons
-import { faUserLarge } from '@fortawesome/free-solid-svg-icons';
+//import { faUserLarge } from '@fortawesome/free-solid-svg-icons';
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
+
+// <FontAwesomeIcon icon={faUserLarge} className='user-icon' />  for profile (logged in)
+// <FontAwesomeIcon icon={faRightFromBracket} /> for signout
 
 import './menuUserProfile.css';
 
@@ -8,7 +12,7 @@ const ProfileButton = () => {
     return (
       <>
         <div className='profile-bttn-container'>
-          <FontAwesomeIcon icon={faUserLarge} className='user-icon' />
+          <FontAwesomeIcon icon={faRightToBracket} className='user-icon'/>
         </div>
       </>
     );
