f4aaba2fdf6c1d99a860963f1ac32fbec6f953aa
Sattyik Kundu <sattyikkundu@gmail.com>
2025-07-12 17:20:46 -0400

    Temporary update to database.js in '/server' to validate connection between Render(express backend) and Neon(database) services.

--- server/database/database.js
+++ server/database/database.js
@@ -7,12 +7,20 @@ import dotenv from 'dotenv'; // loads .env variables into process.env
 dotenv.config();
 
 // Create pool of database connections for PostgreSQL (later move this in an .env file, like in realworld App)
+/*
 const pool = new Pool({
     user:     process.env.DB_USER,        // your postgre database's username
     host:     process.env.DB_HOST,        // since it works on my local computer
     database: process.env.DB_NAME,        // name of database
     password: process.env.DB_PASSWORD,    // password to access database
     port:     process.env.DB_PORT         // port where postgreSQL database is accessible 
+});*/
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false  // Required for Neon on Render
+  }
 });
 
 export default pool;
