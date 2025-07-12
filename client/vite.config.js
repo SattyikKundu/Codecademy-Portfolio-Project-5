1eaa7b807b38e3d0d7508b313cf0c0078b69d053
Sattyik Kundu <sattyikkundu@gmail.com>
2025-07-12 19:18:07 -0400

    Attempted to fix issue where cors in server.js didn't point to CLIENT_HOME_URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'; // Imported to enable React for vite (
 export default defineConfig({
   plugins: [react()], // register vite plugins here (only 'react' needed right now)
   base: '/',          // needed for Render and production paths
-  server: {
+  server: {           // NOTE: this shouldn't run in a production environment and should ONLY run on localhost
     port: 3000,         // frontend server port for localhost
     open: true          // (optional) auto-opens browser when server starts
   }
